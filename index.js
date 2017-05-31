2a2f66198ba4c249a8773f0abf1407a70748383e
Steven Myers <steven.myers@microquest.ca>
2017-05-31 08:01:11 -0600

    add index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+/* I truly despise this library... nuke at the very first oportunity */
+require('./dist/angular-validator-rules')
+require('./angular-validator')
+
+module.exports = 'angular-validator';
